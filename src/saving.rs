4a283d26c658cda628191ff7b73d226700c2adb0
zane-burke <zaneburke418@gmail.com>
2024-07-13 23:43:36 -0500

    added cases for when no data exists in the cache (i.e., a new save file)

--- src/saving.rs
+++ src/saving.rs
@@ -1,12 +1,12 @@
-use serde::{Serialize, Deserialize};
 use chrono::{DateTime, Local};
-use std::time::Duration;
+use serde::{Deserialize, Serialize};
 use std::fs;
+use std::time::Duration;
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Solve {
-    pub timestamp: DateTime<Local>, 
-    pub solve_time: Duration, 
+    pub timestamp: DateTime<Local>,
+    pub solve_time: Duration,
     pub shuffle_used: String,
 }
 
@@ -21,7 +21,7 @@ impl Solve {
 }
 
 pub fn read_save() -> Vec<Solve> {
-    let savefile = fs::read_to_string("save.json").expect("Couldn't read the JSON save file");
+    let savefile = fs::read_to_string("save.json").expect("Couldn't read the JSON save file. This likely means the file doesn't exist or was moved. Just put it back or create a new save.json in the same root directory, and it should work again.");
     let json = serde_json::from_str(&savefile);
     match json {
         Ok(j) => j,
@@ -40,13 +40,19 @@ pub fn save_solves(cache: &Vec<Solve>) {
     fs::write("save.json", json).expect("Couldn't write to file.");
 }
 
-// This should probably be changed to return the median, rather than the mean. 
+// This should probably be changed to return the median, rather than the mean.
 // That would unfortunately require a clone, so I am hesitant to do that.
 pub fn all_time_average(cache: &[Solve]) -> Duration {
     let itered = cache.iter().map(|slv| slv.solve_time);
-    itered.sum::<Duration>() / cache.len() as u32
+    match itered.sum::<Duration>().checked_div(cache.len() as u32) {
+        Some(s) => s,
+        None => Duration::ZERO,
+    }
 }
 
 pub fn personal_best(cache: &[Solve]) -> Duration {
-    cache.iter().map(|slv| slv.solve_time).min().unwrap()
+    match cache.iter().map(|slv| slv.solve_time).min() {
+        Some(pb) => pb,
+        None => Duration::ZERO,
+    }
 }
