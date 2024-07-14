53af3daf98263a1cae62acbf48a5c0107e44d564
zane-burke <zaneburke418@gmail.com>
2024-07-13 20:07:53 -0500

    overhauled how logging works to remove the dedicated log button and merge it with the toggle button

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,25 @@
 mod shuffling;
 
-use iced::widget::{container, Scrollable};
 use iced::window::Position;
 use iced::{alignment, executor, keyboard, time, window, Length};
 use iced::{
-    widget::{button, column, row, text, Slider},
+    widget::{button, column, container, row, text, Scrollable, Slider},
     Application, Command, Element, Font, Settings, Subscription, Theme,
 };
+use itertools::Itertools;
 use shuffling::shuffler;
 use std::time::{Duration, Instant};
 
 use chrono::{DateTime, Local};
 
-const INSPECTION_TIME: Duration = Duration::from_secs(15);
-
+const INSPECTION_TIME: Duration = Duration::from_secs(1);
+const DEFAULT_SHUFFLE_LENGTH: u64 = 25;
+const BIG_FONT_SIZE: u16 = 64;
 fn main() -> iced::Result {
     let settings = Settings {
         window: window::Settings {
             size: iced::Size {
-                width: 500.0,
+                width: 520.0,
                 height: 400.0,
             },
             min_size: Some(iced::Size {
@@ -41,7 +42,6 @@ fn main() -> iced::Result {
 enum Message {
     Toggle,
     Reset,
-    LogSolve,
     Tick(Instant),
     InspectionTick(Instant),
     SliderChanged(f64),
@@ -66,6 +66,7 @@ struct CubeTimer {
 enum State {
     #[default]
     Idle,
+    Finished,
     Inspecting(Instant),
     Ticking(Instant),
 }
@@ -81,9 +82,8 @@ impl Application for CubeTimer {
             Self {
                 inspection: INSPECTION_TIME,
                 finished: false,
-                shuffle_length: 25f64,
-                shuffle: shuffler(25),
-                recent: String::from("\n\n\n\n\n"),
+                shuffle_length: DEFAULT_SHUFFLE_LENGTH as f64,
+                shuffle: shuffler(DEFAULT_SHUFFLE_LENGTH),
                 ..Default::default()
             },
             Command::none(),
@@ -102,17 +102,80 @@ impl Application for CubeTimer {
                 } else {
                     match self.state {
                         State::Idle => {
-                            if self.finished == true {
+                            if !self.finished && self.solve_time > Duration::ZERO {
+                                self.finished = true;
+                                self.state = State::Finished;
                             } else {
-                                if self.finished == false && self.solve_time > Duration::ZERO {
-                                    self.finished = true;
-                                } else {
-                                    self.state = State::Ticking(Instant::now())
-                                }
+                                self.state = State::Ticking(Instant::now())
                             }
                         }
-                        State::Inspecting(..) => {}
-                        State::Ticking(..) => self.state = State::Idle,
+                        State::Finished => {
+                            self.cache
+                                .push((Local::now(), self.solve_time, self.shuffle.clone()));
+
+                            self.reset();
+
+                            let last_five: Vec<&Duration> = self
+                                .cache
+                                .iter()
+                                .rev()
+                                .take(5)
+                                .map(|(_t, st, _sh)| st)
+                                .collect();
+
+                            let (min, max) =
+                                last_five.iter().copied().minmax().into_option().unwrap(); // better than separate min max calls
+
+                            let sum: Duration = last_five.iter().copied().sum();
+
+                            let mut str_accumulator = String::new();
+                            let length = last_five.len(); // necessary because take takes UP TO 5 elements, not necessarily always 5
+
+                            if length == 1 {
+                                let secs = last_five[0].as_secs();
+                                let string = format!(
+                                    "{:0>2}:{:0>2}.{:0>2}",
+                                    (secs % 60) / 60,
+                                    secs % 60,
+                                    last_five[0].subsec_millis() / 10,
+                                );
+
+                                str_accumulator += &string;
+                            } else {
+                                for t in last_five {
+                                    let tsecs = t.as_secs();
+                                    str_accumulator += &format!(
+                                        "{:0>2}:{:0>2}.{:0>2}",
+                                        (tsecs % 60) / 60,
+                                        tsecs % 60,
+                                        t.subsec_millis() / 10,
+                                    );
+
+                                    if t == min {
+                                        str_accumulator += " (Best)\n";
+                                    } else if t == max {
+                                        str_accumulator += " (Worst)\n";
+                                    } else {
+                                        str_accumulator += "\n";
+                                    }
+                                }
+                            }
+
+                            self.recent = str_accumulator;
+                            self.avg = match length {
+                                1 => Duration::from_secs(0),
+                                2 => Duration::from_secs(0),
+                                3 => sum - *max - *min, // removes best and worst runs, leaving 1 solve left
+                                4 => (sum - *max - *min) / 2,
+                                5 => (sum - *max - *min) / 3,
+                                _ => unreachable!("The length can't exceed 5"),
+                            };
+                        }
+                        State::Inspecting(..) => {} // empty to prevent user actions other than resetting
+                        State::Ticking(..) => {
+                            self.state = State::Finished;
+                            self.finished = true;
+                        }
                     }
                 }
             }
@@ -156,69 +219,7 @@ impl Application for CubeTimer {
                         .push((Local::now(), self.solve_time, self.shuffle.clone()));
                 }
 
-                self.inspection = INSPECTION_TIME;
-                self.solve_time = Duration::default();
-                self.finished = false;
-                self.shuffle = shuffler(self.shuffle_length as u64);
-                // Add:
-                // - Thing to cache results
-                // - Thing to reset the previously given shuffle.
-            }
-            Message::LogSolve => {
-                self.cache
-                    .push((Local::now(), self.solve_time, self.shuffle.clone()));
-                self.inspection = INSPECTION_TIME;
-                self.solve_time = Duration::default();
-                self.finished = false;
-                self.shuffle = shuffler(self.shuffle_length as u64);
-
-                let mut last_five: Vec<&Duration> = self
-                    .cache
-                    .iter()
-                    .rev()
-                    .take(5)
-                    .map(|(_t, st, _sh)| st)
-                    .collect();
-
-                last_five.sort();
-
-                let length = last_five.len();
-                let sum: Duration = last_five.iter().copied().sum();
-
-                let mut str_accumulator = String::new();
-
-                if length == 1 {
-                    let secs = last_five[0].as_secs();
-                    let string = format!(
-                        "{:0>2}:{:0>2}.{:0>2}",
-                        (secs % 60) / 60,
-                        secs % 60,
-                        last_five[0].subsec_millis() / 10,
-                    );
-
-                    str_accumulator += &string;
-                } else {
-                    for (i, t) in last_five.into_iter().enumerate() {
-                        let tsecs = t.as_secs();
-                        str_accumulator += &format!(
-                            "{:0>2}:{:0>2}.{:0>2}",
-                            (tsecs % 60) / 60,
-                            tsecs % 60,
-                            t.subsec_millis() / 10,
-                        );
-
-                        if i == 0 {
-                            str_accumulator += " (Best)\n";
-                        } else if i == length - 1 {
-                            str_accumulator += " (Worst)\n";
-                        } else {
-                            str_accumulator += "\n";
-                        }
-                    }
-                }
-
-                self.recent = str_accumulator;
-                self.avg = sum / length as u32;
+                self.reset()
             }
         }
 
@@ -228,6 +229,7 @@ impl Application for CubeTimer {
     fn subscription(&self) -> Subscription<Message> {
         let tick = match self.state {
             State::Idle => Subscription::none(),
+            State::Finished => Subscription::none(),
             State::Inspecting(..) => {
                 time::every(Duration::from_millis(1)).map(Message::InspectionTick)
             }
@@ -237,9 +239,8 @@ impl Application for CubeTimer {
         fn handle_hotkey(key: keyboard::Key, _mods: keyboard::Modifiers) -> Option<Message> {
             use keyboard::key;
             match key.as_ref() {
-                keyboard::Key::Named(key::Named::Space) => Some(Message::Toggle),
+                keyboard::Key::Named(key::Named::Space) => Some(Message::Toggle), // consider adding an if-else block that triggers LogSolve instead of Toggle
                 keyboard::Key::Named(key::Named::Escape) => Some(Message::Reset),
-                keyboard::Key::Character("l") => Some(Message::LogSolve),
                 keyboard::Key::Named(key::Named::ArrowRight) => Some(Message::IncrementSlider),
                 keyboard::Key::Named(key::Named::ArrowLeft) => Some(Message::DecrementSlider),
                 _ => None,
@@ -259,34 +260,38 @@ impl Application for CubeTimer {
             ..Font::default()
         };
 
-        let inspection_label = text("Inspection Timer")
-            .font(font_bf)
-            .horizontal_alignment(alignment::Horizontal::Left);
+        let timer_label = if self.inspection > Duration::ZERO {
+            text("Inspection Timer")
+                .font(font_bf)
+                .horizontal_alignment(alignment::Horizontal::Left)
+        } else {
+            text("Solve Timer")
+                .font(font_bf)
+                .horizontal_alignment(alignment::Horizontal::Left)
+        };
 
-        let inspection_time = self.inspection.as_secs();
+        let timer = if self.inspection > Duration::ZERO {
+            let inspection_time = self.inspection.as_secs();
 
-        let inspection_timer = text(format!(
-            "{:0>2}.{:0>2}",
-            inspection_time % MIN,
-            self.inspection.subsec_millis() / 10,
-        ))
-        .horizontal_alignment(alignment::Horizontal::Left);
+            text(format!(
+                "{:0>2}.{:0>2}",
+                inspection_time % MIN,
+                self.inspection.subsec_millis() / 10,
+            ))
+            .size(BIG_FONT_SIZE)
+            .horizontal_alignment(alignment::Horizontal::Left)
+        } else {
+            let solve_time = self.solve_time.as_secs();
 
-        let solve_label = text("Solve Timer")
-            .font(font_bf)
-            .horizontal_alignment(alignment::Horizontal::Left);
-
-        let solve_time = self.solve_time.as_secs();
-
-        let solve_timer = text(format!(
-            "{:0>2}:{:0>2}.{:0>2}",
-            (solve_time % HR) / MIN,
-            solve_time % MIN,
-            self.solve_time.subsec_millis() / 10,
-        ))
-        .horizontal_alignment(alignment::Horizontal::Left);
-
-        let button = |label| button(text(label)).padding(10).width(Length::Fill);
+            text(format!(
+                "{:0>2}:{:0>2}.{:0>2}",
+                (solve_time % HR) / MIN,
+                solve_time % MIN,
+                self.solve_time.subsec_millis() / 10,
+            ))
+            .size(BIG_FONT_SIZE)
+            .horizontal_alignment(alignment::Horizontal::Left)
+        };
 
         let toggle_button = {
             let label = if self.inspection == INSPECTION_TIME {
@@ -294,6 +299,7 @@ impl Application for CubeTimer {
             } else {
                 match self.state {
                     State::Idle => "Start Solve (Space)",
+                    State::Finished => "Log Solve (Space",
                     State::Inspecting(..) => "Inspecting!",
                     State::Ticking(..) => "Stop (Space)",
                 }
@@ -302,11 +308,11 @@ impl Application for CubeTimer {
             button(label).on_press(Message::Toggle).width(Length::Fill)
         };
 
-        let discard_button = button("Discard and Reset (Esc)").on_press(Message::Reset);
+        let discard_button = button("Discard and Reset (Esc)")
+            .on_press(Message::Reset)
+            .width(Length::Fill);
 
-        let log_button = button("Log and Reset (L)").on_press(Message::LogSolve);
-
-        let controls = column![toggle_button, log_button, discard_button].spacing(10);
+        let controls = column![toggle_button, discard_button].spacing(10);
 
         let avg_label = text("Average:").font(font_bf);
 
@@ -339,17 +345,14 @@ impl Application for CubeTimer {
         let keybind_text =
             text("You can use the left and right arrows to change the shuffle length.");
 
-        row![
-            column![
-                inspection_label,
-                inspection_timer,
-                solve_label,
-                solve_timer,
-                controls,
-                keybind_text,
+        column![
+            timer_label,
+            timer,
+            row![
+                column![controls, keybind_text,].spacing(10),
+                column![avg_container, five_container, shuffle].spacing(10)
             ]
-            .spacing(10),
-            column![avg_container, five_container, shuffle].spacing(10)
+            .spacing(10)
         ]
         .spacing(10)
         .padding(10)
@@ -358,5 +361,14 @@ impl Application for CubeTimer {
 
     fn theme(&self) -> Theme {
         Theme::CatppuccinMacchiato
+    }
+}
+
+impl CubeTimer {
+    fn reset(&mut self) {
+        self.inspection = INSPECTION_TIME;
+        self.solve_time = Duration::default();
+        self.finished = false;
+        self.shuffle = shuffler(self.shuffle_length as u64);
     }
 }
