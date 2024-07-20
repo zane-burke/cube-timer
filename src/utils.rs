309ce6e6fb039ced4b71c989e4b08ab9fb3e5f9c
Zane Burke <zaneburke418@gmail.com>
2024-07-20 02:08:21 -0400

    added a couple new functions and moved time-related constants here as well

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,13 @@
 //! Several useful functions that don't require direct access to anything
 
-use crate::timer::INSPECTION_TIME;
+pub const INSPECTION_TIME: u64 = 15_000;
 
-pub fn get_current_time() -> String {
-    let date = js_sys::Date::new_0();
-    date.get_time().to_string()
+const SECOND: u64 = 1_000;
+const MINUTE: u64 = 60 * SECOND;
+const HOUR: u64 = 60 * MINUTE;
+
+pub fn get_current_time() -> u64 {
+    js_sys::Date::new_0().get_time() as u64
 }
 
 pub fn saturating_sub(lhs: u64, rhs: u64) -> u64 {
@@ -25,7 +28,7 @@ pub fn saturating_div(lhs: u64, rhs: u64) -> u64 {
 
 // decreases time
 pub fn dec_time(start_time: Option<u64>) -> u64 {
-    let c = js_sys::Date::new_0().get_time() as u64;
+    let c = get_current_time();
     let s = start_time.unwrap_or(0) + INSPECTION_TIME;
 
     saturating_sub(s, c)
@@ -33,7 +36,7 @@ pub fn dec_time(start_time: Option<u64>) -> u64 {
 
 // increases time
 pub fn inc_time(start_time: Option<u64>) -> u64 {
-    let c = js_sys::Date::new_0().get_time() as u64;
+    let c = get_current_time();
     let s = start_time.unwrap_or(0);
 
     c - s
@@ -42,4 +45,23 @@ pub fn inc_time(start_time: Option<u64>) -> u64 {
 /// splits the shuffle sequence into vecs of length 5
 pub fn chunk_vec(shuffle: &Vec<String>) -> Vec<Vec<String>> {
     shuffle.chunks(5).map(|s| s.into()).collect()
+}
+
+/// formats a `u64` time into a pretty string
+pub fn time_string(time: u64) -> String {
+    let min = (time / MINUTE) % 60; // convert time to the right unit then mod 60 to prevent overflow
+    let s = (time / SECOND) % 60;
+    let ms = (time % SECOND) / 10;
+
+    if time >= HOUR {
+        let hr = time / HOUR;
+        return format!("{:0>2}:{:0>2}:{:0>2}.{:0>2}", hr, min, s, ms).to_string();
+    }
+
+    format!("{:0>2}:{:0>2}.{:0>2}", min, s, ms).to_string()
+}
+
+/// handles `Option<u64>` - `Option<u64>`
+pub fn saturating_unwrap_sub(lhs: Option<u64>, rhs: Option<u64>) -> u64 {
+    saturating_sub(lhs.unwrap_or(0), rhs.unwrap_or(0))
 }
