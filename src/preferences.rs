6a82243d69cca6121bd71bdedd7cd59ad1d24b54
Zane Burke <zaneburke418@gmail.com>
2024-07-19 23:46:36 -0400

    overhauled to avoid having to prop drill

--- src/preferences.rs
+++ src/preferences.rs
@@ -6,38 +6,46 @@ use serde::{Deserialize, Serialize};
 
 const PREFERENCES_KEY: &str = "zane-burke.cube-timer.preferences.self";
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Copy)]
 pub struct Preferences {
     pub shuffle_length: u64,
     pub dark_mode: bool,
 }
 
-impl Preferences {
-    /// this should honestly probably be done with the `Default` trait
-    /// since its purpose is the same, but I'm lazy so I won't.
-    pub fn new() -> Self {
+impl Default for Preferences {
+    fn default() -> Self {
         Self {
             shuffle_length: 25,
             dark_mode: false,
         }
     }
+}
 
-    /// setter for the theme
-    /// Also updates the preferences in local storage
-    pub fn set_theme(&mut self, dark: bool) {
-        self.dark_mode = dark;
-        LocalStorage::set(PREFERENCES_KEY, self).expect("Failed to update theme preference");
-    }
+/// setter for the theme
+/// Also updates the preferences in local storage
+pub fn set_theme(dark: bool) {
+    LocalStorage::set(PREFERENCES_KEY, dark).expect("Failed to update theme preference");
+}
 
-    /// setter for the shuffle length
-    /// Also updates the preferences in local storage
-    pub fn set_length(&mut self, length: u64) {
-        self.shuffle_length = length;
-        LocalStorage::set(PREFERENCES_KEY, self).expect("Failed to update length preference");
-    }
+/// getter for the theme
+pub fn get_theme() -> bool {
+    let prefs = LocalStorage::get(PREFERENCES_KEY).unwrap_or_else(|_| Preferences::default());
+    prefs.dark_mode
+}
 
-    /// retrieves preferences from local storage
-    pub fn retrieve_preferences() -> Self {
-        LocalStorage::get(PREFERENCES_KEY).unwrap_or_else(|_| Self::new())
-    }
+/// setter for the shuffle length
+/// Also updates the preferences in local storage
+pub fn set_length(length: u64) {
+    LocalStorage::set(PREFERENCES_KEY, length).expect("Failed to update length preference");
+}
+
+/// getter for the shuffle length
+pub fn get_length() -> u64 {
+    let prefs = LocalStorage::get(PREFERENCES_KEY).unwrap_or_else(|_| Preferences::default());
+    prefs.shuffle_length
+}
+
+/// retrieves preferences from local storage
+pub fn get_preferences() -> Preferences {
+    LocalStorage::get(PREFERENCES_KEY).unwrap_or_else(|_| Preferences::default())
 }
