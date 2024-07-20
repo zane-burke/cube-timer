2125e2f2c22710ba1892d46b0d381a835e59ec3c
Zane Burke <zaneburke418@gmail.com>
2024-07-20 01:24:11 -0400

    added ShuffleDisplay and changed default to use local storage values

--- src/shuffle.rs
+++ src/shuffle.rs
@@ -1,7 +1,10 @@
-use web_sys::HtmlInputElement;
-use yew::NodeRef;
+//! Handles shuffle generation
 
-use crate::seq_gen;
+use gloo::utils::document;
+use web_sys::{HtmlInputElement, Node};
+use yew::{function_component, Html, NodeRef, Properties};
+
+use crate::{preferences, seq_gen};
 
 pub struct Shuffle {
     pub sequence: Vec<String>,
@@ -12,9 +15,11 @@ pub struct Shuffle {
 
 impl Default for Shuffle {
     fn default() -> Self {
+        let length = preferences::get_length();
+        
         Self {
-            sequence: seq_gen::shuffler(25),
-            length: 25,
+            sequence: seq_gen::shuffler(length),
+            length,
             error: String::new(),
             node_ref: NodeRef::default(),
         }
@@ -30,10 +35,11 @@ impl Shuffle {
             Ok(r) => {
                 self.error.clear();
                 self.length = r;
+                preferences::set_length(r);
             },
             Err(e) => {
                 if length_value.is_empty() {
-                    self.length = 25;
+                    self.length = preferences::get_length();
                 } else {
                     self.error = format!("Invalid input: {}", e);
                 }
@@ -42,4 +48,53 @@ impl Shuffle {
 
         self.sequence = seq_gen::shuffler(self.length);
     }
+}
+
+#[derive(Properties, PartialEq)]
+pub struct ShuffleDisplayProps {
+    pub shuffle: Vec<Vec<String>>,
+    pub dark: bool,
+}
+
+#[function_component]
+pub fn ShuffleDisplay(props: &ShuffleDisplayProps) -> Html {
+    let shuffle = &props.shuffle;
+
+    let node = {
+        let table = document().create_element("table").unwrap();
+        table.class_list().add_1("shuffle-table").unwrap();
+
+        if props.dark {
+            table.class_list().add_1("dark").unwrap();
+        }
+
+        let table_body = document().create_element("tbody").unwrap();
+
+        shuffle.iter().for_each(|row_data| {
+            let row = document().create_element("tr").unwrap();
+
+            row_data.iter().for_each(|cell_data| {
+                let cell = document().create_element("td").unwrap();
+                
+                cell.class_list().add_1("shuffle-cell").unwrap();
+
+                if props.dark {
+                    cell.class_list().add_1("dark").unwrap();
+                }
+
+                cell.append_child(&document().create_text_node(cell_data))
+                    .unwrap();
+                row.append_child(&cell).unwrap();
+            });
+
+            table_body.append_child(&row).unwrap();
+        });
+
+        table.append_child(&table_body).unwrap();
+
+        let node: Node = table.into();
+        Html::VRef(node)
+    };
+
+    node
 }
